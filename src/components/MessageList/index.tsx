6dd251c3745189d008e412e9feb631137d3d259d
Lucas Tello <lucas.sta_@hotmail.com>
2021-10-20 09:37:52 -0300

    First commit

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from 'react';
+import { api } from '../../services/api';
+import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3')
+      .then(response => {
+        console.log(response.data);
+        setMessages(response.data);
+      })
+  }, []);
+
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {
+          messages.map(message => {
+            return (
+              <li className={styles.message}>
+                <p className={styles.messageContent}>
+                  {message.text}
+                </p>
+                <div className={styles.messageUser}>
+                  <div className={styles.userImg}>
+                    <img src={message.user.avatar_url} alt={message.user.name} />
+                  </div>
+                  <span>{message.user.name}</span>
+                </div>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
   );
 }
